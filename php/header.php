4ab15276b878ffec33015b88e2ed0f7829023225
EdvaldoJr <jredvaldo@hotmail.com>
2021-10-14 22:28:17 -0300

    LayoutEstruturado

---
+++ php/header.php
@@ -0,0 +1,37 @@
+<img class="img-fluid col-12 p-0 m-0" id="header-banner" src="img/banner.png" alt="Banner superior">
+        <div class="d-flex" id="wrapper">    
+            <!-- Menu -->
+            <div id="page-content-wrapper">
+                <!-- Top navigation-->
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom">
+                    <div class="container-fluid">
+                        
+                        <!-- menu button  -->
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                        <!-- Search button -->
+
+                        <form class="d-class">
+                            <div class="input-group d-flex">
+                                <input type="text" class="form-control" placeholder="Concursos..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="bi bi-search"></i></button>
+                            </div>
+                        </form>
+                        <!-- itens do menu -->
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                                <li class="nav-item active"><a class="nav-link" href="#!"><i class="bi bi-house"></i> Home</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#!"><i class="bi bi-link-45deg"></i> Link</a></li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="bi bi-person-square"></i> Perfil</a>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#!"><i class="bi bi-box-arrow-in-right"></i> Entrar</i></a>
+                                        <a class="dropdown-item" href="#!"><i class="bi bi-stack"></i> Meus planos</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#!"><i class="bi bi-box-arrow-left"></i> Sair</a>
+                                    </div>
+                                </li>
+                            </ul>    
+                        </div>
+                    </div>
+                </nav>
+                

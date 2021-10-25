73366869ba37f8a41b07949c77f8668f2e6696fd
EdvaldoJr <jredvaldo@hotmail.com>
2021-10-24 22:17:21 -0300

    MenusESecoes

--- php/topmenu.php
+++ php/topmenu.php
@@ -11,30 +11,35 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="#"><i class="bi bi-house"></i> Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-book"></i> Cursos</a></li>
-                <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-people-fill"></i> Concursos</a>
+                <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-person-circle"></i> Aluno</a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#"><i class="bi bi-pencil-square"></i> ENEM</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-columns-gap"></i> Painel do aluno</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-signpost-split"></i> Roteiro de estudos</a></li>
                         <li><a class="dropdown-item" href="#"><i class="bi bi-calendar-day"></i> Agenda do candidato</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-collection"></i> Meus planos</a></li>
                         <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Outros Concursos</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-pencil-square"></i> Outros concursos</a></li>
                     </ul>
                 </li>
                 
-                <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarEstudos" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-ui-checks"></i> Estudos</a>
+                <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarEstudos" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-people"></i> ENEM</a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#"><i class="bi bi-tv"></i> Video aulas</a></li>
-                        <li><a class="dropdown-item" href="#"><i class="bi bi-list-check"></i> Exercícios resolvidos</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-distribute-horizontal"></i> Edital verticalizado</a></li>
                         <li><a class="dropdown-item" href="#"><i class="bi bi-newspaper"></i> Plano de estudos</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-hand-thumbs-up"></i> ENEM resolvido</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-files"></i> Disciplinas</a></li>        
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-tv"></i> Video aulas</a></li>    
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-ui-checks"></i> Assuntos estudados</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-patch-question"></i> Tira dúvidas</a></li>
+                        
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="#"><i class="bi bi-layers-half"></i></i> Simulados</a></li>
                         <li><a class="dropdown-item" href="#"><i class="bi bi-clipboard-check"></i> Correção de provas</a></li>
                         <li><a class="dropdown-item" href="#"><i class="bi bi-inboxes"></i> Provas anteriores</a></li>
+                        <li><a class="dropdown-item" href="#"><i class="bi bi-check-circle"></i> Exercícios resolvidos</a></li>
                         <li><a class="dropdown-item" href="#"><i class="bi bi-server"></i> Banco de dados de provas</a></li>
                     </ul>
-                </li>
-                
-                
-                
+                </li> 
             </ul>
             <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Vídeo aula..." aria-label="Buscar">
